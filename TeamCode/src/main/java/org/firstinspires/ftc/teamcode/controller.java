ad51d9c724aac860017452e66f841eec26a6f39d
Rodrichhhhh <andykyrykos@gmail.com>
2025-11-09 15:58:39 +0200

    Code,update,version,2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controller.java
@@ -1,165 +1,117 @@
 package org.firstinspires.ftc.teamcode;
 
-
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import  com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.IMU;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "controller")
 public class controller extends LinearOpMode {
     @Override
     public void runOpMode() {
-        //set up motors and variables
-        DcMotor TopLeftMotor, BottomLeftMotor, TopRightMotor, BottomRightMotor, Collector, AssistantShooter;
-        DcMotorEx shooterMotor;
+
+        // Gamepad tracking for toggles
+        Gamepad currentGamepad2 = new Gamepad();
+        Gamepad previousGamepad2 = new Gamepad();
+
+
+        // Motors
+        DcMotor TopLeftMotor, BottomLeftMotor, TopRightMotor, BottomRightMotor;
+        DcMotor Collector, AssistantShooter;
+        DcMotorEx ShooterMotor;
         IMU imu;
 
-
+        // Initialize hardware
         TopLeftMotor = hardwareMap.get(DcMotor.class, "TopLeftMotor");
         BottomLeftMotor = hardwareMap.get(DcMotor.class, "BottomLeftMotor");
         TopRightMotor = hardwareMap.get(DcMotor.class, "TopRightMotor");
         BottomRightMotor = hardwareMap.get(DcMotor.class, "BottomRightMotor");
         Collector = hardwareMap.get(DcMotor.class, "Collector");
-        shooterMotor = hardwareMap.get(DcMotorEx.class, "Shooter");
+        ShooterMotor = hardwareMap.get(DcMotorEx.class, "Shooter");
         AssistantShooter = hardwareMap.get(DcMotor.class, "AssistantShooter");
+
         imu = hardwareMap.get(IMU.class, "imu");
 
-
-        shooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
+        // Motor directions
         TopLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-//        BottomRightMotor.setDirection(DcMotor.Direction.REVERSE);
-
-
         Collector.setDirection(DcMotor.Direction.REVERSE);
 
+        // IMU setup
+        RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.DOWN,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);     //FORWARDS somewhat works
+        imu.initialize(new IMU.Parameters(orientation));
 
-        TopLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BottomLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        TopRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BottomRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        ShooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
-        double PER_REV = 28;
-        //double HALF_POWER = -0.5
-        double MAX_POWER = -1;
-        double NO_POWER = 0;
-        double SevenFive_Power = -1;
+        double MAX_POWER = -1;           //power used by motors
+        double NO_POWER = 0;             //power of motors when inactive
+        double PER_REV = 28;             //REV rotations
+        boolean intakeToggle = false;    //Shooter
+        boolean intakeToggle2 = false;   //Assistant Shooter
+        boolean intakeToggle3 = false;   //Collector
+
+        while (opModeIsActive()) {
+            previousGamepad2.copy(currentGamepad2);
+            currentGamepad2.copy(gamepad2);
+
+            // Intake toggle
+            if (currentGamepad2.right_bumper && !previousGamepad2.right_bumper) {
+                intakeToggle = !intakeToggle;
+            }
+            ShooterMotor.setPower(intakeToggle ? MAX_POWER : NO_POWER);
+
+            //assistant shooter
+            if (currentGamepad2.a && !previousGamepad2.a) {
+                intakeToggle2 = !intakeToggle2;
+            }
+            AssistantShooter.setPower(intakeToggle2 ? MAX_POWER : NO_POWER);
 
 
-        while (opModeIsActive()){
-
-            // COLLECTOR
-            if (gamepad2.y) {
-                Collector.setPower(-1);  // adjust speed if needed
+            //collector
+            if (currentGamepad2.y && !previousGamepad2.y) {
+                intakeToggle3 = !intakeToggle3;
 
             } else {
-                Collector.setPower(0);  // stop when button released
+                Collector.setPower((0));
             }
-
-
-            // SHOOTER
-            if (gamepad2.right_bumper) {
-                shooterMotor.setPower(MAX_POWER);
+            Collector.setPower(intakeToggle3 ? MAX_POWER : NO_POWER);
 
 
 
-            } else {
-                shooterMotor.setPower(NO_POWER); // stop shooter
-
-
-            }
-
-            //if (gamepad2.left_bumper) {
-                //shooterMotor.setPower(HALF_POWER);
-            //} else {
-                //shooterMotor.setPower(NO_POWER); // stop shooter
-
-           // }
-
-
-
-
-
-            double velocity = shooterMotor.getVelocity(); // ticks/second
-            double shooterRPM = (velocity / PER_REV) * 60;
-
-            telemetry.addData("Shooter Power", "%.2f", shooterMotor.getPower());
-            telemetry.addData("Shooter RPM", "%.0f", shooterRPM);
-            telemetry.update();
-
-            //AssistantShooter
-
-            if (gamepad2.a) {
-                AssistantShooter.setPower(-1);
-            }  else {
-                AssistantShooter.setPower(0);
-            }
-
-            //MOVING
+            // Drive control
             double rotate = gamepad1.right_stick_x;
             double strafe = gamepad1.left_stick_x;
             double forward = -gamepad1.left_stick_y;
 
-            TopLeftMotor.setPower(forward + rotate + strafe);
-            BottomLeftMotor.setPower(forward + rotate - strafe);
-            TopRightMotor.setPower(forward - rotate - strafe);
-            BottomRightMotor.setPower(forward - rotate + strafe);
+            double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double r = Math.hypot(strafe, forward);
+            double theta = Math.atan2(forward, strafe) - heading;
+
+            //Crazy Math
+            double newForward = r * Math.sin(theta);
+            double newStrafe = r * Math.cos(theta);
+
+            TopLeftMotor.setPower(newForward + rotate + newStrafe);
+            BottomLeftMotor.setPower(newForward + rotate - newStrafe);
+            TopRightMotor.setPower(newForward - rotate - newStrafe);
+            BottomRightMotor.setPower(newForward - rotate + newStrafe);
 
 
-//            double TopLeftPower = forward + strafe + rotate;
-//            double BottomLeftPower = forward - strafe + rotate;
-//            double TopRightPower = forward - strafe - rotate;
-//            double BottomRightPower = forward + strafe - rotate;
-//
-//            double maxPower = 1.0;
-//            double maxSpeed = 1.0;
-//
-//            maxPower = Math.max(maxPower, Math.abs(TopLeftPower));
-//            maxPower = Math.max(maxPower, Math.abs(BottomLeftPower));
-//            maxPower = Math.max(maxPower, Math.abs(TopRightPower));
-//            maxPower = Math.max(maxPower, Math.abs(BottomRightPower));
-//
-//            TopLeftMotor.setPower(maxSpeed * (TopLeftPower / maxPower));
-//            BottomLeftMotor.setPower(maxSpeed * (BottomLeftPower / maxPower));
-//            TopRightMotor.setPower(maxSpeed * (TopRightPower / maxPower));
-//            BottomRightMotor.setPower(maxSpeed * (BottomRightPower / maxPower));
-            RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
-                    RevHubOrientationOnRobot.LogoFacingDirection.DOWN,
-                    RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
-            //🐥
-            imu.initialize(new IMU.Parameters(RevOrientation));
+            double velocity = ShooterMotor.getVelocity(); // ticks/second
+            double shooterRPM = (velocity / PER_REV) * 60;
 
-                double theta = Math.atan2(forward, strafe);
-                double r = Math.hypot(strafe, forward);
-
-                theta = AngleUnit.normalizeRadians(theta -
-                        imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
-
-                double newForward = r * Math.sin(theta);
-                double newStrafe = r * Math.cos(theta);
-
-
+            //RPM display calculations
+            telemetry.addData("Shooter Power", "%.2f", ShooterMotor.getPower());
+            telemetry.addData("Shooter RPM", "%.0f", shooterRPM);
+            telemetry.update();
         }
-
-
-
-
-
-        }
-
-
-
     }
-
-
-
-
+}
---
+++ connect.cmd
@@ -0,0 +1,2 @@
+cd C:\Users\Admin\AppData\Local\Android\Sdk\platform-tools
+adb connect 192.168.43.1
